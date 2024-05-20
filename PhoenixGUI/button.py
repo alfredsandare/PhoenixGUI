eee55909ccb9fa29a4b6ced47344a2be6d2955af
alfredsandare <alfred.s.sandare04@gmail.com>
2024-05-20 16:01:49 +0200

    wip Button refactor

--- PhoenixGUI/button.py
+++ PhoenixGUI/button.py
@@ -80,8 +80,108 @@ class Button(MenuObject):
         
         if self.rect_length is not None and self.rect_padx != 0:
             raise Exception("Cannot instantiate button with both rect_length and rect_padx")
+        
+        if self.enable_rect and self.text is None and \
+            (self.rect_length is None or self.rect_height is None):
+            raise Exception("Button instantiated without text must have rect_length and rect_height.")
+        
+    def _get_rendered_image(self, menu_pos, menu_size, ui_size, scroll):
+        if self.image is None:
+            return None
+        
+        image_to_use = get_value_from_state(self.state,
+                                            self.image,
+                                            self.hover_image,
+                                            self.click_image)
 
+        menu_image = Image((0, 0), 
+                            image_to_use, 
+                            max_size=self.max_size, 
+                            anchor=self.anchor)
+        
+        return menu_image.render(menu_pos, menu_size, ui_size, scroll)
+    
+    def _get_rendered_text(self, menu_pos, menu_size, ui_size, scroll):
+        if self.text is not None:
+            return None
+        
+        text_color_to_use = get_value_from_state(self.state,
+                                                 self.text_color,
+                                                 self.text_hover_color,
+                                                 self.text_click_color)
+
+        menu_text = Text((0, 0), 
+                            self.text, 
+                            self.font, 
+                            self.font_size, 
+                            max_size=self.max_size,
+                            wrap_lines=False,
+                            color=text_color_to_use)
+        
+        return menu_text.render(menu_pos, menu_size, ui_size, scroll)
+    
+    def _get_rendered_rect(self, menu_pos, menu_size, ui_size, scroll):
+        if self.enable_rect:
+            return None
+
+        rect_color = get_value_from_state(self.state,
+                                            self.rect_color,
+                                            self.rect_hover_color,
+                                            self.rect_click_color)
+
+        rect_outline_color = \
+            get_value_from_state(self.state,
+                                    self.rect_outline_color,
+                                    self.rect_outline_hover_color,
+                                    self.rect_outline_click_color)
+
+        menu_rect = Shape((0, 0),
+                            rect_size,
+                            rect_color,
+                            "rect",
+                            outline_width=self.rect_outline_width,
+                            outline_color=rect_outline_color,
+                            border_radius=self.rect_border_radius,
+                            max_size=self.max_size)
+        
+        return menu_rect.render(menu_pos, menu_size, ui_size, scroll)
+    
+        
     def render(self, menu_pos, menu_size, ui_size, scroll):
+        rendered_image = self._get_rendered_image(menu_pos, menu_size, 
+                                                  ui_size, scroll)
+        
+        rendered_text = self._get_rendered_text(menu_pos, menu_size, 
+                                                ui_size, scroll)
+        
+        text_size = rendered_text.get_image_size()
+        
+        # enable rect
+        # rect length, rect height
+        # rect padx
+
+        # Case 1: Rect, no text (image does not matter here): Rect size comes from rect_length and rect_height
+        # Case 2.1: Rect, text, rect_length: Rect length comes from rect_length
+        # Case 2.2: Rect, text, rect_heigth: Rect heigth comes from rect_height
+        ### Case 2.1 and 2.2 can be combined
+
+        # Case 3: Rect, text, no rect_length or rect_height: Rect size comes from text size and perhaps from padding.
+
+        rect_size = [0, 0]
+        if self.enable_rect and self.text is None:
+            rect_size = [self.rect_length, self.rect_height]
+
+        elif (self.enable_rect and self.text is not None 
+              and self.rect_length is not None 
+              and self.rect_length < text_size[0]):
+            rect_size[0] = self.rect_length
+
+
+        
+        rendered_rect = self._get_rendered_rect(menu_pos, menu_size, 
+                                                ui_size, scroll)
+
+    def render2(self, menu_pos, menu_size, ui_size, scroll):
         if self.image is not None:
             image_to_use = get_value_from_state(self.state,
                                                 self.image,
@@ -119,7 +219,7 @@ class Button(MenuObject):
             if self.text:
                 text_size = rendered_menu_text.get_image_size()
                 rect_size = [text_size[0] + 2*self.rect_padx, 
-                            text_size[1] + 2*self.rect_pady]
+                             text_size[1] + 2*self.rect_pady]
 
                 if self.rect_length is not None and rect_size[0] < self.rect_length:
                     rect_size[0] = self.rect_length
@@ -167,7 +267,7 @@ class Button(MenuObject):
         if self.enable_rect:
             surface.blit(rendered_menu_rect.image, (0, 0))
         if self.text:
-           surface.blit(rendered_menu_text.image, text_pos)
+            surface.blit(rendered_menu_text.image, text_pos)
 
         crop, pos = self._adjust_pos_and_crop(self.pos, surface_size, 
                                               menu_pos, menu_size, scroll)
