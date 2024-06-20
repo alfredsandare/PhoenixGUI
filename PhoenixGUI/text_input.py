92d9d2588e31a98ec3d99bb02e2bf0a0df09c113
Alf <alfred.s.sandare04@gmail.com>
2024-06-20 12:17:04 +0200

    added validity check for text_input

--- PhoenixGUI/text_input.py
+++ PhoenixGUI/text_input.py
@@ -3,6 +3,12 @@ from .rendered_menu_object import RenderedMenuObject
 from .util import get_font
 from .menu_object import MenuObject
 
+class validity_check:
+    NATURAL = 100  # 0, 1, 2, 3, ...
+    INTEGERS = 101 # -3, -2, -1, 0, 1, 2, 3, ...
+    ALL_NUMBERS_COMMA = 102 # -3, -2, -1, 0, 1, 2, 3, ..., 0.1, 0.2, 0.3, ... using comma signs to indicate decimals
+    ALL_NUMBERS_DOTS = 103 # -3, -2, -1, 0, 1, 2, 3, ..., 0.1, 0.2, 0.3, ... using dots to indicate decimals
+    ALL_NUMBERS_DOTS_COMMA = 104 # -3, -2, -1, 0, 1, 2, 3, ..., 0.1, 0.2, 0.3, ... using dots or comma signs to indicate decimals
 
 class TextInput(MenuObject):
     def __init__(self,
@@ -10,7 +16,9 @@ class TextInput(MenuObject):
                  length,
                  font,
                  font_size,
-                 text_color,
+                 text_color=(255, 255, 255),
+                 validity_check=None,
+                 invalid_text_color=(255, 0, 0),
                  max_size=None,
                  anchor="nw",
                  layer=0,
@@ -21,11 +29,14 @@ class TextInput(MenuObject):
         self.font = font
         self.font_size = font_size
         self.text_color = text_color
+        self.validity_check = validity_check
+        self.invalid_text_color = invalid_text_color
 
         self.text_left = ""
         self.text_right = ""
         self.is_selected = False
         self.offset = 0
+        self.is_valid = True
 
     def render(self, menu_pos, menu_size, ui_size, scroll, font_path=None):
         font = get_font(font_path, self.font, self.font_size)
@@ -33,7 +44,8 @@ class TextInput(MenuObject):
         surface_size = (self.length, font.size(" ")[1])
         surface = pygame.Surface(surface_size, pygame.SRCALPHA)
 
-        rendered_text = font.render(self.get_text(), True, self.text_color)
+        text_color = self.text_color if self.is_valid else self.invalid_text_color
+        rendered_text = font.render(self.get_text(), True, text_color)
         surface.blit(rendered_text, (-self.offset, 0))
 
         if self.is_selected:
@@ -54,6 +66,9 @@ class TextInput(MenuObject):
         if font.size(self.text_left)[0] - self.offset > self.length:
             self.offset += font.size(text)[0]
 
+        self.render_flag = True
+        self.check_validity()
+
     def remove_text(self):
         if not self.text_left:
             return
@@ -68,6 +83,7 @@ class TextInput(MenuObject):
         self.text_left = self.text_left[:-1]
 
         self.render_flag = True
+        self.check_validity()
 
     def step_right(self):
         if not self.text_right:
@@ -99,4 +115,29 @@ class TextInput(MenuObject):
         self.render_flag = True
 
     def get_text(self):
+        return (self.text_left + self.text_right).strip()
+    
+    def get_text_raw(self):
         return self.text_left + self.text_right
+
+    def check_validity(self):
+        if self.validity_check == validity_check.NATURAL:
+            self.is_valid = self.get_text().isdigit()
+
+        elif self.validity_check == validity_check.INTEGERS:
+            self.is_valid = self.get_text().lstrip("-").isdigit()
+            
+        elif self.validity_check == validity_check.ALL_NUMBERS_COMMA:
+            self.is_valid = self.get_text().replace(".", "").lstrip("-").replace(",", "", 1).isdigit()
+
+        elif self.validity_check == validity_check.ALL_NUMBERS_DOTS:
+            self.is_valid = self.get_text().replace(",", "").lstrip("-").replace(".", "", 1).isdigit()
+
+        elif self.validity_check == validity_check.ALL_NUMBERS_DOTS_COMMA:
+            # disallow more than one dot or comma
+            text = self.get_text()
+            self.is_valid = text.replace(",", "").replace(".", "").lstrip("-").isdigit() \
+                            and text.count(".") + text.count(",") <= 1
+            
+    def get_validity(self):
+        return self.is_valid
